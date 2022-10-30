697243807f120b43e95e7c3608b523708b0249ff
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-10-30 16:29:09 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,9 @@ function App() {
   const count = useSelector(state => state.counter.count);
   const dispatch = useDispatch();
   const vh = window.innerHeight;
-  console.log(vh,'hey');
 
   return (
-    <div className="App" style={{marginTop:`${0.4*vh}px`,position:"relative",marginLeft:"40%",width:"250px",height:"200px", borderWidth:"5px", borderStyle:"solid", borderColor:"green"}}>
+    <div className="App gradient__bg" style={{marginTop:`${0.4*vh}px`,position:"relative",marginLeft:"40%",width:"250px",height:"200px", borderWidth:"5px", borderStyle:"solid"}}>
 
       <h1>Counter {count}</h1>
       <button onClick={() => dispatch(increment())} style = {{margin:'5px','paddingRight':'20px','paddingLeft':'20px'}}>+</button>
